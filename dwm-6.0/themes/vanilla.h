e9c3c23411467a658b78712b60a971311dc807b6
KD0SKH <patrick91jh@gmail.com>
2016-12-28 20:29:01 -0700

    Initial commit

---
+++ dwm-6.0/themes/vanilla.h
@@ -0,0 +1,11 @@
+// Classic Theme
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#229977";
+static const char selbgcolor[]      = "#116655";
+static const char selfgcolor[]      = "#eeeeee";
+static const char dmenuselbgcolor[]      = "#992244";
+static const char dmenuselfgcolor[]      = "#ffffff";
+static const char floatnormbordercolor[] = "#992244";
+static const char floatselbordercolor[]  = "#aa2299";
---
+++ dwm-6.0/themes/when_destiny_meets_oblivion.h
@@ -0,0 +1,13 @@
+// A theme that corresponds to the artwork of the same name orange and blue
+// theme
+
+static const char normbordercolor[]       = "#444444";
+static const char normbgcolor[]           = "#522f23"; 
+static const char normfgcolor[]           = "#fd9127"; 
+static const char selbordercolor[]        = "#7985ba"; 
+static const char selbgcolor[]            = "#fe9227"; 
+static const char selfgcolor[]            = "#6d5e5f"; 
+static const char dmenuselbgcolor[]       = "#992244";
+static const char dmenuselfgcolor[]       = "#ffffff";
+static const char floatnormbordercolor[]  = "#664444";
+static const char floatselbordercolor[]   = "#b12a02";
