8f8d8d09d9de933a2663eaf35a69c393419f51a1
KO0I <patrick91jh@gmail.com>
2017-04-17 19:27:44 -0600

    renaming and tidying up a little

---
+++ src/themes/mountain.h
@@ -0,0 +1,14 @@
+// Corresponds to: https://imgur.com/cflQHZy
+// Mountains by Dylan Furst
+
+static const char normbordercolor[] = "#5b5a5d";
+static const char normbgcolor[]     = "#2c3c42";
+static const char normfgcolor[]     = "#bbbbbb";
+//static const char selbordercolor[]  = "#dae2dc";
+static const char selbordercolor[]  = "#1db954";
+static const char selbgcolor[]      = "#ecf2f2";
+static const char selfgcolor[]      = "#5e024a";
+static const char dmenuselbgcolor[]      = "#bf585e";
+static const char dmenuselfgcolor[]      = "#ffffff";
+static const char floatnormbordercolor[] = "#b7aba7";
+static const char floatselbordercolor[]  = "#dec69a";
