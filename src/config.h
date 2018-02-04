21d96d0a56607416a71c247034ff290418bf9934
KO0I <patrick@teshub.localdomain>
2018-02-03 22:22:04 -0700

    minor edits

--- src/config.h
+++ src/config.h
@@ -9,15 +9,15 @@
 //  static const char font[]       = "terminus-10";
   static const char font[]              = "-*-terminus-medium--r-*-*-14-*-*-*-*-*-*-*";
 //  static const char font[]            = "-xos4-terminus-medium-*-normal-*-14-140-72-72-c-80-iso10646-1";
-  static const char dmenufont[]         = "-*-terminus-medium--r-*-*-*-14-*-*-*-*-*-*-*";
-//  static const char dmenufont[]       = "terminus-10";
+//  static const char dmenufont[]         = "-*-terminus-medium--r-*-*-*-14-*-*-*-*-*-*-*";
+  static const char dmenufont[]       = "terminus-10";
 //  static const char dmenufont[]       = "-xos4-terminus-medium-*-normal-*-14-140-72-72-c-80-iso10646-1";
 //#endif
 //static const char font[]            = "-*-inconsolata--r-*-*-20-*-*-*-*-*-*-*";
 //#include <themes/when_destiny_meets_oblivion.h>
 #include <themes/parts_unknown.h>
 static const unsigned int borderpx  = 2;    /* border pixel of windows */
-static const unsigned int gappx     = 20;        /* gap pixel between windows */
+unsigned int gappx     = 20;        /* gap pixel between windows */
 static const unsigned int snap      = 32;   /* snap pixel */
 static const unsigned int minwsz    = 5;
 static const Bool showbar           = True; /* False means no bar */
